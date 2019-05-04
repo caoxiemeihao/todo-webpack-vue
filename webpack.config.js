d61b20452acbd2717f22093e10409d9139fe6b38
Atom <308487730@qq.com>
2019-05-04 16:25:34 +0800

    Configure webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const {VueLoaderPlugin} = require('vue-loader')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  mode: isDev ? 'development' : 'production',
   entry: path.join(__dirname, 'src/main.js'),
   output: {
     filename: 'bundle.js',
@@ -40,6 +44,32 @@ module.exports = {
    * 报错解决 [https://www.imooc.com/notepad/224b3f]
    */
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLWebpackPlugin()
   ]
 }
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',            // IP、localhost、127.0.0.1 均能访问
+    overlay: {
+      errors: true              // 报错信息丢屏幕上
+    },
+    // open: true,              // 自动打开浏览器
+    // historyApiFallback: {},  // 将所有找不到的路由统一重定向回入口
+    hot: true,                  // 局部刷新
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+}
+
+module.exports = config
