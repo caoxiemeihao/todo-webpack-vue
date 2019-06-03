a5bd63ca95e0fb80f6b118958f701d4c728eb776
草鞋没号 <308487730@qq.com>
2019-06-03 09:21:25 +0800

    19-06-03 edit Header、footer components.

--- src/app.vue
+++ src/app.vue
@@ -1,23 +1,43 @@
 <template>
-    <div id="test">{{text}}</div>
+  <div id="app">
+    <div id="cover"></div>
+    <Header/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-  import './assets/styles/test.css'
-  import './assets/styles/test-stylus.styl'
-  import  './assets/images/bg.jpg'
+import Header from "./todo/header.vue";
+import Footer from "./todo/footer.jsx";
 
-  export default {
-    data() {
-      return {
-        text: 'Hello world.'
-      }
-    }
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Footer
   }
+};
 </script>
 
-<style>
-    #test {
-        color: #ff6470;
-    }
+<style lang="stylus" scoped>
+#app {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  #cover {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: url('./assets/images/jump.jpg');
+    // filter: blur(4px);
+    z-index: -1;
+  }
+}
 </style>
