9d2cdf8b866ffca26c0086eeeafee9f31fe9ac0e
草鞋没号 <308487730@qq.com>
2019-06-09 13:09:27 +0800

    css 抽离.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
-const {VueLoaderPlugin} = require('vue-loader')
+const { VueLoaderPlugin } = require('vue-loader')
 const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +10,7 @@ const config = {
   mode: isDev ? 'development' : 'production',
   entry: path.join(__dirname, 'src/main.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -25,20 +26,6 @@ const config = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options:  {
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -69,6 +56,20 @@ const config = {
 }
 
 if (isDev) {
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     port: 8000,
@@ -83,6 +84,30 @@ if (isDev) {
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    // webpack中的hash、chunkhash、contenthash
+    // new ExtractPlugin('styles.[contenthash:8].css') // 报错
+    // new ExtractPlugin('styles.[chunkhash:8].css')
+    new ExtractPlugin('styles.[hash:8].css')
   )
 }
 
